c87d4293cf7fca557b09ce2ae89e324c6054c84a
Leedoyi <doyi9917@naver.com>
2022-01-20 11:15:10 +0900

    [Login page change]

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -17,11 +17,52 @@ import BeplainSlideBox from "../components/BeplainSlideBox";
 function Main () {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <TestDiv />
-      
+      <section id="viewBox">
+        <h2>
+          <span className="blind">브랜드별 제품을 슬라이드로 소개합니다.</span>
+        </h2>
 
-      {/* <Footer /> */}
+        {/* 피부타입 테스트 */}
+        <div className="skin_type_test_btn">
+          <button type="button">
+            <Link to="/skin-type-test">피부타입 테스트</Link>
+          </button>
+        </div>
+
+        <section className="dr_jart_box">
+          <h3>
+            <span className="blind">닥터자르트 제품 보기</span>
+          </h3>
+          <DaJartSlideBox/>
+          
+  
+        </section>
+        <section className="innisfree_box">
+          <h3>
+            <span className="blind">이니스프리 제품 보기</span>
+          </h3>
+        <InnisfreeSlideBox />
+        </section>
+        <section className="seedmool_box">
+          <h3>
+            <span className="blind">시드물 제품 보기</span>
+          </h3>
+        <SeedmoolSlideBox/>
+
+        </section>
+        <section className="beplain_box">
+          <h3>
+            <span className="blind">비플레인 제품 보기</span>
+          </h3>
+          <BeplainSlideBox/>
+          
+        </section>
+
+      </section>
+
+      <Footer />
     </>
   )
 }
