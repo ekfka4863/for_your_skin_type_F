36087911244af366607150555cddec83157bd6a2
ekfka4863 <ekfka4863@gmail.com>
2022-01-24 14:27:09 +0900

    [Remove] all files related to redux

--- src/pages/MyCart.js
+++ src/pages/MyCart.js
@@ -1,22 +1,10 @@
 // 공통 컴포넌트 임포트 
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-import Card from "../../components/Card";
+import Card from "../components/Card";
 
-import "../../styles/src/MyCart.scss";
-
-// redux & reducer 
-import { connect } from "react-redux";   // store에 있는 state 데이터를 꺼내기 위해 필요!
-
-// store에 있는 state 데이터를 꺼내기 위해 필요한 함수
-// const mapStateToProps = (state) => {
-//   return (
-//     myCart: state.cart.myCart,
-//   )
-// };
-
-
+import "../styles/src/MyCart.scss";
 
 
 // 진짜 컴포넌트 
@@ -26,7 +14,7 @@ function MyCart () {
       <Header />
 
       <div id="myCartBox">
-        <h2 className="blind">환영합니다! gusw위시리스트를 해당 페이지에서 확인하세요.</h2>  
+        <h2 className="blind">환영합니다! 위시리스트를 해당 페이지에서 확인하세요.</h2>  
         <div className="favorite_items_total_num">
           <h3>장바구니</h3>
           {/* <div className="total_num">Total: {___} item(s)</div> */}
