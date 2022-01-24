7eeb41863ca7f2f2a124288a88d26def4c6c6f09
ekfka4863 <ekfka4863@gmail.com>
2022-01-24 13:42:53 +0900

    [Add] interim commit before git pull from doyi's remote origin repo

--- src/pages/MyCart/MyCart-action.js
+++ src/pages/MyCart/MyCart-action.js
@@ -1,36 +1,36 @@
-import * as actionTypes from "../MyCart/MyCart-types";
+// import * as actionTypes from "../MyCart/MyCart-types";
 
-export const addToCart = (itemID) => {
-  return (
-    type: "ADD_TO_CART",
-    payload: {
-      id: itemID
-    }
-  )
-};
+// export const addToCart = (itemID) => {
+//   return (
+//     type: "ADD_TO_CART",
+//     payload: {
+//       id: itemID
+//     }
+//   )
+// };
 
-export const removeFromCart = (itemID) => {
-  return (
-    type: actionTypes.REMOVE_FROM_CART,
-    payload: {
-      id: itemID
-    }
-  )
-};
+// export const removeFromCart = (itemID) => {
+//   return (
+//     type: actionTypes.REMOVE_FROM_CART,
+//     payload: {
+//       id: itemID
+//     }
+//   )
+// };
 
-export const CountQty = (itemID, value) => {
-  return (
-    type: actionTypes.COUNT_QTY,
-    payload: {
-      id: itemID, 
-      qty: value
-    }
-  )
-};
+// export const CountQty = (itemID, value) => {
+//   return (
+//     type: actionTypes.COUNT_QTY,
+//     payload: {
+//       id: itemID, 
+//       qty: value
+//     }
+//   )
+// };
 
-export const loadCurrentItem = (item) => {
-  return (
-    type: actionTypes.LOAD_CURRENT_ITEM,
-    payload: item
-  )
-};
+// export const loadCurrentItem = (item) => {
+//   return (
+//     type: actionTypes.LOAD_CURRENT_ITEM,
+//     payload: item
+//   )
+// };
